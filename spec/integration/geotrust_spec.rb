afcc22d01ea8f0fe4434943f18d189c94ae6703c
Olle Jonsson <olle.jonsson@auctionet.com>
2022-01-25 16:15:17 +0100

    Upgrade to RSpec 3

--- spec/integration/geotrust_spec.rb
+++ spec/integration/geotrust_spec.rb
@@ -6,12 +6,11 @@ describe 'Integration with Geotrust' do
 
   it 'knows the operations' do
     pending "this fixture is missing a message element! " \
-            "find out if we need to handle this case or if the fixture is incomplete." do
+            "find out if we need to handle this case or if the fixture is incomplete."
 
-      operation = client.operation('GetQuickApproverList')
-      expect(operation.name).to eq('THIS-TEST-FAILS')
-      expect(operation.endpoint).to eq('https://test-api.geotrust.com:443/webtrust/query.jws')
-    end
+    operation = client.operation('GetQuickApproverList')
+    expect(operation.name).to eq('THIS-TEST-FAILS')
+    expect(operation.endpoint).to eq('https://test-api.geotrust.com:443/webtrust/query.jws')
   end
 
 end
